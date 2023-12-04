25174de5212189a44e25725ccb29191ec322530e
nicolesundberg <nicole.sundberg@utah.edu>
2023-12-04 00:18:31 -0700

    added rotation for teleportation to tent and field

--- Assets/TeleportBehavior.cs
+++ Assets/TeleportBehavior.cs
@@ -7,16 +7,23 @@ public class TeleportBehavior : MonoBehaviour
     // Start is called before the first frame update
 
     Vector3 tentPos = new Vector3(-58.09f, 11.256f, 70.94f);
+
+    public GameObject screen;
+    public GameObject board;
+
     Vector3 startPos;
     public Camera myCam;
     Vector3 lastPos;
 
     bool inTent = false;
 
+    Vector3 boardPos;
+
 
     void Start()
     {
         lastPos = startPos = myCam.transform.position;
+        boardPos = new Vector3(board.transform.position.x, tentPos.y, board.transform.position.z);
     }
 
     // Update is called once per frame
@@ -28,11 +35,17 @@ public class TeleportBehavior : MonoBehaviour
             {
                 //put player into the startPos
                 this.transform.position -= (myCam.transform.position - startPos);
+                this.transform.RotateAround(myCam.transform.position, Vector3.up, 180);
+                //this.transform.LookAt(boardPos, Vector3.up);
             }
             //lastPos = myCam.transform.position;
             else
             {
                 this.transform.position -= (myCam.transform.position - tentPos);
+                this.transform.RotateAround(myCam.transform.position, Vector3.up, 180);
+
+                //this.transform.LookAt(screen.transform.position, Vector3.up);
+
             }
 
             inTent = !inTent;
