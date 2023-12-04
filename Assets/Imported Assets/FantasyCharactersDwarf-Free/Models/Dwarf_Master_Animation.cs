8dbcbd5af3e545c4762c407b76c2a5fcaf0b3d41
Mickey-Cloud <u1117450@umail.utah.edu>
2023-12-03 23:33:31 -0700

    animation files

---
+++ Assets/Imported Assets/FantasyCharactersDwarf-Free/Models/Dwarf_Master_Animation.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dwarf_Master_Animation : MonoBehaviour
+{
+    public Animator animator;
+    public float DwarfMaterMovePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator= this.gameObject.GetComponent<Animator>();
+        DwarfMaterMovePos = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("DwarfInput", DwarfMaterMovePos);
+        
+    }
+}
